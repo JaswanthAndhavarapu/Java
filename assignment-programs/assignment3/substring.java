568eb6c8c888af51d0a3d3ff3df5435c2b870632
JaswanthAndhavarapu <andhavarapujaswanth.25.csm@anits.edu.in>
2026-09-10 23:07:45 +0530

    Create substring.java

---
+++ assignment-programs/assignment3/substring.java
@@ -0,0 +1,11 @@
+class SubstringDemo {
+    public static void main(String[] args) {
+        String str = "Programming";
+        
+        System.out.println("Original = " + str);
+        System.out.println("From index 3 = " + str.substring(3));
+        System.out.println("From index 4 = " + str.substring(4));
+        System.out.println("From index 5 = " + str.substring(5));
+        System.out.println("substring() extracts part of string");
+    }
+}
