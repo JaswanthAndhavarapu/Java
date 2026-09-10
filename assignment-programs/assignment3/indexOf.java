ca561bfaf17688eea3543acfcea38869366c175c
JaswanthAndhavarapu <andhavarapujaswanth.25.csm@anits.edu.in>
2026-09-10 23:10:46 +0530

    Add IndexDemo class to demonstrate indexOf method

---
+++ assignment-programs/assignment3/indexOf.java
@@ -0,0 +1,11 @@
+class IndexDemo {
+    public static void main(String[] args) {
+        String str = "Java Programming";
+        
+        System.out.println("String = " + str);
+        System.out.println("Index of Java = " + str.indexOf("Java"));
+        System.out.println("Index of Program = " + str.indexOf("Program"));
+        System.out.println("Index of a = " + str.indexOf("a"));
+        System.out.println("Index of Python = " + str.indexOf("Python"));
+    }
+}
