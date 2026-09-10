295705d85af7beb9bf40b6a0e9794c4abfe0cd50
JaswanthAndhavarapu <andhavarapujaswanth.25.csm@anits.edu.in>
2026-09-10 23:00:12 +0530

    Add LengthDemo class to demonstrate string length

---
+++ assignment-programs/assignment3/Length.java
@@ -0,0 +1,12 @@
+class LengthDemo {
+    public static void main(String[] args) {
+        String str = "Java Programming";
+        int n = str.length();
+
+        System.out.println("String = " + str);
+        System.out.println("Length = " + n);
+        System.out.println("Characters = " + n);
+        System.out.println("String length is calculated");
+        System.out.println("Using length() method");
+    }
+}
