c7d856d79fda8426d17cf105af21eb36191d10ff
JaswanthAndhavarapu <andhavarapujaswanth.25.csm@anits.edu.in>
2026-09-10 23:02:56 +0530

    Add toUpperCase.java for string conversion example

---
+++ assignment-programs/assignment3/toUpperCase.java
@@ -0,0 +1,11 @@
+class UpperDemo {
+    public static void main(String[] args) {
+        String str = "Java Programming";
+        
+        System.out.println("Original = " + str);
+        System.out.println("Uppercase = " + str.toUpperCase());
+        System.out.println("Original String = " + str);
+        System.out.println("Method used: toUpperCase()");
+        System.out.println("Conversion completed");
+    }
+}
