49231e389326d975a3b7f80c9ebb1070e905c35f
JaswanthAndhavarapu <andhavarapujaswanth.25.csm@anits.edu.in>
2026-09-10 23:13:05 +0530

    Add StartsDemo class to demonstrate startsWith method

---
+++ assignment-programs/assignment3/startsWith.java
@@ -0,0 +1,11 @@
+class StartsDemo {
+    public static void main(String[] args) {
+        String str = "Java Programming";
+        
+        System.out.println("String = " + str);
+        System.out.println("Starts with Java = " + str.startsWith("Java"));
+        System.out.println("Starts with Pro = " + str.startsWith("Pro"));
+        System.out.println("Starts with Python = " + str.startsWith("Python"));
+        System.out.println("startsWith() checks beginning");
+    }
+}
