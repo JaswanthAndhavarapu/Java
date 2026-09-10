090ae478c531a8cfca3e22dee69f01db19d71760
JaswanthAndhavarapu <andhavarapujaswanth.25.csm@anits.edu.in>
2026-09-10 23:01:23 +0530

    Add charAt.java to demonstrate charAt method

---
+++ assignment-programs/assignment3/charAt.java
@@ -0,0 +1,12 @@
+class CharAtDemo {
+    public static void main(String[] args) {
+        String str = "Java";
+        
+        System.out.println("String = " + str);
+        System.out.println("Character at 0 = " + str.charAt(0));
+        System.out.println("Character at 1 = " + str.charAt(1));
+        System.out.println("Character at 2 = " + str.charAt(2));
+        System.out.println("Character at 3 = " + str.charAt(3));
+        System.out.println("Using charAt() method");
+    }
+}
