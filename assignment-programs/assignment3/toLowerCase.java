141c8547e06975e8d339a8d66202a9f5a530ee54
JaswanthAndhavarapu <andhavarapujaswanth.25.csm@anits.edu.in>
2026-09-10 23:04:42 +0530

    Add toLowerCase.java to demonstrate string conversion

---
+++ assignment-programs/assignment3/toLowerCase.java
@@ -0,0 +1,11 @@
+class LowerDemo {
+    public static void main(String[] args) {
+        String str = "JAVA PROGRAMMING";
+        
+        System.out.println("Original = " + str);
+        System.out.println("Lowercase = " + str.toLowerCase());
+        System.out.println("Original String = " + str);
+        System.out.println("Method used: toLowerCase()");
+        System.out.println("Conversion completed");
+    }
+}
