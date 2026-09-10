79ca12a3f12a8c1ea83d6c0b75b0b0f1134985c0
JaswanthAndhavarapu <andhavarapujaswanth.25.csm@anits.edu.in>
2026-09-10 23:10:13 +0530

    Add ContainsDemo class to showcase string contains method
    Demonstrate the use of the contains() method in Java strings.

---
+++ assignment-programs/assignment3/contains.java
@@ -0,0 +1,11 @@
+class ContainsDemo {
+    public static void main(String[] args) {
+        String str = "Java Programming";
+        
+        System.out.println("String = " + str);
+        System.out.println("Contains Java = " + str.contains("Java"));
+        System.out.println("Contains Program = " + str.contains("Program"));
+        System.out.println("Contains Python = " + str.contains("Python"));
+        System.out.println("contains() returns true or false");
+    }
+}
