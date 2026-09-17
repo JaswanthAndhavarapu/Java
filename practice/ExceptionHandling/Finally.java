75a72f3178ccef12cd5a708551a9033a3366616d
JaswanthAndhavarapu <andhavarapujaswanth.25.csm@anits.edu.in>
2026-09-17 22:02:31 +0530

    Add FinallyDemo class to demonstrate finally block

---
+++ practice/ExceptionHandling/Finally.java
@@ -0,0 +1,14 @@
+public class FinallyDemo {
+    public static void main(String[] args) {
+        try {
+            int a = 10 / 0;
+            System.out.println(a);
+        }
+        catch (ArithmeticException e) {
+            System.out.println("Cannot divide by zero");
+        }
+        finally {
+            System.out.println("Finally block executed");
+        }
+    }
+}
